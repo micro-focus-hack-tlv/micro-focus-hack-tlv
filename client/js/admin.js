875eff9781b779b3662f46d5bdca5db84aee6bfb
Uri Kalish <uri.kalish@microfocus.com>
2019-03-27 18:08:00 +0200

    wip

--- client/js/admin.js
+++ client/js/admin.js
@@ -41,21 +41,15 @@ goBroarcastToMobiles = () => {
 };
 
 onServerMsg = (data) => {
-    console.log(`onServerMsg(${data.msg})`);
-    alert(data.msg);
+    console.log(`onServerMsg(${data.msg})`);    
 };
 
 onUserListUpdate = (data) => {
     console.log(`onUserListUpdate(${data.msg})`);
-    alert(`user list update ${userNames.join()}`);
-
     let namesElement = document.getElementById('user-names');
-
-    // Clear the users list
     while (namesElement.hasChildNodes()) {
         namesElement.removeChild(namesElement.firstChild);
     }
-
     userNames.forEach((user) => {
         var liElement = document.createElement('div');
         liElement.innerText = user;
