4d92ff053cc4369cc7c1c05515330d7a3d53148b
saribiv <saribiv@gmail.com>
2019-03-29 01:30:26 +0200

    wip

--- client/js/mobile-mime.js
+++ client/js/mobile-mime.js
@@ -14,7 +14,7 @@ showMessagesFromMimeGame = (data) => {
     console.log('Phrase: ' + data.selectedPhrase);
 
     if(data.userName === userName) {
-		document.getElementById("mime-header").innerText = 'יאללה ' + userName +'\n!...אתה מציג';
+		document.getElementById("mime-header").innerText = 'יאללה ' + userName +'\n!...את\\ה מציג\\ה';
         document.getElementById('phrase').innerText = data.selectedPhrase;
         document.getElementById('phraseFoundButton').style.visibility = "visible";
         document.getElementById('mime-center-circle').style.visibility = "visible";
@@ -22,7 +22,7 @@ showMessagesFromMimeGame = (data) => {
         document.getElementById('mobile-mime-container').classList.remove('image-background');
         document.getElementById("mime-header").classList.remove('mime-not-chosen');
     } else {
-        document.getElementById("mime-header").innerText = data.userName + ' מציג\n אתם מנחשים';
+        document.getElementById("mime-header").innerText = data.userName + ' מציג\\ה\n אתם מנחשים';
         document.getElementById('phraseFoundButton').style.visibility = "hidden";
         document.getElementById('mime-center-circle').style.visibility = "hidden";
         document.getElementById('mobile-mime-container').classList.add('mime-image-background');
